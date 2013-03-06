07c3471304d2c7443fe7db0b10a80265cb8e3900
Andy Hume <andyhume@gmail.com>
2013-03-06 16:15:17 +0000

    Fixes for non srcdoc browsers.

--- app/scripts/embed.js
+++ app/scripts/embed.js
@@ -2,27 +2,35 @@ define(function() {
 
 	var count = 0;
 
-	function embedDone(id) {
-        setTimeout(function() {
-            resizeIframe(id);
-        }, 1000);
+	function embedDone(id, wait) {
+		console.log(id);
+		setTimeout(function() {
+			resizeIframe(id);
+		}, (wait || 0));
     }
 
     function enhance() {
 	    var embeds = document.querySelectorAll('iframe.api-embed:not(.api-embed-polyfilled)');
-
+	    console.log(embeds);
 	    for (var i = 0, j = embeds.length; i<j; ++i) {
-	        var id = 'embed-iframe-' + count++;
+	        var id = 'embed-iframe-' + ++count;
 	        var iframe = embeds[i];
 	        iframe.id = id;
 	        var supportsSrcdoc = iframe.srcdoc;
-	        var src = iframe.getAttribute('srcdoc');
-	        src += '<script>window.parent.embedDone("' + id + '");<' + '/script>';
+	        var src = iframe.getAttribute('srcdoc')
 
 	        if (!supportsSrcdoc) {
+	        	src += '<script>window.parent.embedDone("' + id + '", 1000);<' + '/script>';
 	        	iframe.contentWindow.document.write(src);
 	        } else {
-	        	iframe.srcdoc = src;
+	        	if (iframe.contentWindow.document.readyState === 'complete') {
+	        		embedDone(id);
+	        	} else {
+	        		var thisid = id;
+	        		iframe.addEventListener('load', function() {
+	        			embedDone(thisid);
+	        		}, false);
+	        	}
 	        }
 
 	        iframe.className = iframe.className + ' api-embed-polyfilled';
@@ -30,11 +38,12 @@ define(function() {
 	}
 
 	function resizeIframe(id) {
-	    var iframe = document.getElementById(id)
+	    var iframe = document.getElementById(id);
 	    var b = iframe.contentWindow.document.body;
 	    b.style.padding = 0;
 	    b.style.margin = 0;
 	    var height = iframe.contentWindow.document.height || iframe.contentWindow.document.body.offsetHeight;
+	    console.log(height);
 	    iframe.style.height = height + 'px';
 	}
 
